144e9158cb60231c25c2479fecdb118d8911e0ab
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-09 17:30:56 -0300

    9.13. Content-Type personalizado

--- src/services/Metric.service.ts
+++ src/services/Metric.service.ts
@@ -3,18 +3,45 @@ import Service from "../Service";
 
 class MetricService extends Service {
 
-    static getTop3Tags() {
-        return this.Http
-            .get<Metric.EditorTagRatio>('/metrics/editor/top3-tags')
-            .then(this.getData)
-    }
+  static getTop3Tags() {
+    return this.Http
+      .get<Metric.EditorTagRatio>('/metrics/editor/top3-tags')
+      .then(this.getData)
+  }
+
+  static getEditorMonthlyEarnings() {
+    return this.Http
+      .get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings')
+      .then(this.getData)
+  }
 
 
-    static getEditorMonthlyEarnings() {
-        return this.Http
-            .get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings')
-            .then(this.getData)
-    }
+  static getMonthlyRevenuesExpenses() {
+    return this.Http
+      .get<Metric.MonthlyRevenueExpenses>(
+        `/metrics/montly-revenues-expenses`,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        }
+      )
+      .then(this.getData)
+  }
+
+  static getMonthlyRevenuesExpensesChartJs() {
+    return this.Http
+      .get<Metric.MonthlyRevenueExpenses>(
+        `/metrics/montly-revenues-expenses`,
+        {
+          headers: {
+            'Content-Type': 'application/vnd.alganews.chartjs+json'
+          },
+        }
+      )
+      .then(this.getData)
+  }
+
 
 }
 
