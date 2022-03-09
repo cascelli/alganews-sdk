a34aba5916e1e690c4ba98ce88f64451b0bb97b4
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-09 15:12:21 -0300

    9.12. Desafio - Finaliza o serviço de Posts

--- src/services/Post.service.ts
+++ src/services/Post.service.ts
@@ -4,52 +4,63 @@ import generateQueryString from "../utils/generateQueryString";
 
 class PostService extends Service {
 
-    static getAllPosts(search: Post.Query) {
+  static getAllPosts(search: Post.Query) {
 
-        const queryString = generateQueryString(search)
-        //console.log(queryString) // Mostra a queryString
+    const queryString = generateQueryString(search)
+    //console.log(queryString) // Mostra a queryString
 
-        return this.Http
-            .get<Post.Paginated>('/posts'.concat(queryString)) // concatena a uri posts com a queryString
-            //.then(res => res.data)
-            .then(this.getData) // Simplificacao da linha anterior
-    }
+    return this.Http
+      .get<Post.Paginated>('/posts'.concat(queryString)) // concatena a uri posts com a queryString
+      //.then(res => res.data)
+      .then(this.getData); // Simplificacao da linha anterior
+  }
 
 
-    static getExistingPost(id: number) {
-        return this.Http
-            .get<Post.Detailed>(`/posts/${id}`)
-            //.then(res => res.data)
-            .then(this.getData)  // Simplificacao da linha anterior
-    }
+  static getExistingPost(id: number) {
+    return this.Http
+      .get<Post.Detailed>(`/posts/${id}`)
+      //.then(res => res.data)
+      .then(this.getData);  // Simplificacao da linha anterior
+  }
 
     
-    static insertNewPost(post: Post.Input) {
+  static insertNewPost(post: Post.Input) {
 
-        //console.log('Servico InsertNewPost => post.tags: ', post.tags)
+    //console.log('Servico InsertNewPost => post.tags: ', post.tags)
 
-        return this.Http
-            .post<Post.Detailed>('/posts', post)
-            .then(this.getData)
-    }
-
-
-    static publishExistingPost(postId: number) {
-        return this.Http
-            .put<{}>(`/posts/${postId}/publishing`)
-            .then(this.getData)
-    }
+    return this.Http
+      .post<Post.Detailed>('/posts', post)
+      .then(this.getData);
+  }
 
 
-    static updateExistingPost(postId: number, post: Post.Input) {
+  static publishExistingPost(postId: number) {
+    return this.Http
+      .put<{}>(`/posts/${postId}/publishing`)
+      .then(this.getData);
+  }
 
-        //console.log('Servico UpdateExistingPost => post.tags: ', post.tags)
 
-        return this.Http
-            .put<Post.Detailed>(`/posts/${postId}`, post)
-            .then(this.getData)
-    }
+  static updateExistingPost(postId: number, post: Post.Input) {
 
+    //console.log('Servico UpdateExistingPost => post.tags: ', post.tags)
+
+    return this.Http
+      .put<Post.Detailed>(`/posts/${postId}`, post)
+      .then(this.getData);
+  }
+
+  static deleteExistingPost(postId: number) {
+    return this.Http
+      .delete<{}>(`/posts/${postId}`)
+      .then(this.getData);
+  }
+
+  static deactivateExistingPost(postId: number) {
+    return this.Http
+      .delete<{}>(`/posts/${postId}/activation`)
+      .then(this.getData);
+  }
 
 } 
 
