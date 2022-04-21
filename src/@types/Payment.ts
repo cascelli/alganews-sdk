005c8f9f03b8f828fcf27769e8be39cb779b8788
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-21 11:08:34 -0300

    fix getAllPayments query

--- src/@types/Payment.ts
+++ src/@types/Payment.ts
@@ -1,26 +1,27 @@
 import { AlgaNews } from "./AlgaNews";
 
 export namespace Payment {
-    export type Input = AlgaNews.components['schemas']['PaymentInput']
-    export type Detailed = AlgaNews.components['schemas']['PaymentDetailed']
-    export type Summary = AlgaNews.components['schemas']['PaymentSummary']
+  export type Input = AlgaNews.components["schemas"]["PaymentInput"];
+  export type Detailed = AlgaNews.components["schemas"]["PaymentDetailed"];
+  export type Summary = AlgaNews.components["schemas"]["PaymentSummary"];
 
-    export type Preview = AlgaNews.components['schemas']['PaymentPreview']
-    export type PreviewInput = AlgaNews.components['schemas']['PaymentPreviewInput']
+  export type Preview = AlgaNews.components["schemas"]["PaymentPreview"];
+  export type PreviewInput =
+    AlgaNews.components["schemas"]["PaymentPreviewInput"];
 
-    export type Paginated = AlgaNews.components['schemas']['PaymentsPaginated']
+  export type Paginated = AlgaNews.components["schemas"]["PaymentsPaginated"];
 
-    // export type Earnings = AlgaNews.components['schemas']['PaymentEarnings']
+  // export type Earnings = AlgaNews.components['schemas']['PaymentEarnings']
 
-    export type PostWithEarnings = AlgaNews.components['schemas']['PostWithEarnings']
+  export type PostWithEarnings =
+    AlgaNews.components["schemas"]["PostWithEarnings"];
 
-    export type Query = {
-        payeeId?: number;
-        payeeEmail?: number;
-        scheduledToMonth?: string;
-        page?: number;
-        size?: number;
-        sort: [keyof Summary, 'asc' | 'desc'];
-    };
-
+  export type Query = {
+    payeeId?: number;
+    payeeEmail?: number;
+    scheduledToYearMonth?: string;
+    page?: number;
+    size?: number;
+    sort: [keyof Summary, "asc" | "desc"];
+  };
 }
