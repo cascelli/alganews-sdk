1a266047fa8d3b0e0fe6fbdc80b54f8f13db0380
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-04-28 09:27:16 -0300

    add way to add request interceptors http

--- src/Service.ts
+++ src/Service.ts
@@ -1,31 +1,38 @@
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import handleAxiosResponseError from "./utils/handleAxiosResponseError";
 import handleAxiosResponseSuccess from "./utils/handleAxiosResponseSuccess";
- 
+
 // cria uma instancia do Axios
-const Http = axios.create()
+const Http = axios.create();
 
-// Clase basica para uso em serviços 
+// Clase basica para uso em serviços
 class Service {
+  // protected so pod eser acessada por ela mesma ou pelos herdeiros dessa classe
+  // Isso forçará outras classes a estenderem dela para poder usar os serviços criados
+  protected static Http = Http; // a propriedade protected Http receberá o valor da instancia de Http do Axios
 
-    // protected so pod eser acessada por ela mesma ou pelos herdeiros dessa classe
-    // Isso forçará outras classes a estenderem dela para poder usar os serviços criados
-    protected static Http = Http // a propriedade protected Http receberá o valor da instancia de Http do Axios
+  protected static getData = getData; // Obtem apenas os dados da requisicao para facilitar
 
-    protected static getData = getData // Obem apenas os dados da requisicao para facilitar  
-
+  // Cria um interceptador publico de requisições para uso com API blindada (segurança - Cap 17.10)
+  public static setRequestInterceptors(
+    onFulfilled: (
+      request: AxiosRequestConfig
+    ) => AxiosRequestConfig | Promise<AxiosRequestConfig>, // define AxiosRequestConfig como sincrono ou assincrono
+    onRejected: (error: any) => any
+  ) {
+    Http.interceptors.request.use(onFulfilled, onRejected);
+  }
 }
 
 function getData<T>(res: AxiosResponse<T>) {
-    return res.data // Obtem a propriedade "data" de response (res) de uma requisicao a API do backend
+  return res.data; // Obtem a propriedade "data" de response (res) de uma requisicao a API do backend
 }
 
 // Configura o BaseURL alterando a propriedade protegida criada acima
-Http.defaults.baseURL = 'Http://localhost:8080'
+Http.defaults.baseURL = "Http://localhost:8080";
 
 // Intercepta response do axios
 Http.interceptors.response.use(
-    handleAxiosResponseSuccess,
-    handleAxiosResponseError
+  handleAxiosResponseSuccess,
+  handleAxiosResponseError
 );
-  
